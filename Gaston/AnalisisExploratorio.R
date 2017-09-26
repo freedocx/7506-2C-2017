b83df78a8aeb6dffd1728adb97a08346a2ea203c
Gastón Montes <gmontes@tbxnet.com>
2017-09-26 10:53:47 -0300

    Gaston - Add analysis.

--- Gaston/AnalisisExploratorio.R
+++ Gaston/AnalisisExploratorio.R
@@ -68,4 +68,60 @@ sellsInBsAsWithPrice$text <- dataText$text
 sellsInBsAsWithPrice$title <- NULL
 sellsInBsAsWithPrice$description <- NULL
 
+### Price by month.
+sellsByMonth = sellsInBsAsWithPrice[, c(2,9)]
+sellsByMonth$create_on_month <- month(sellsByMonth$created_on)
 
+months_list <- c("Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre")
+
+sellsGroupedByMonth = sellsByMonth %>% 
+  group_by(create_on_month) %>%
+  summarise(mean_value = mean(price_aprox_usd))
+sellsGroupedByMonth$create_on_month_name = months_list[sellsGroupedByMonth$create_on_month]
+
+ggplot(sellsGroupedByMonth, aes(create_on_month_name, mean_value, fill=create_on_month_name), scale = "free") +
+  geom_bar(stat = "identity", width = 0.6) +
+  xlab("Mes") +
+  ylab("Promedio en miles de d??lares") +
+  ggtitle("Precio de los inmuebles promedio segun mes de creacion") +
+  theme(axis.text.x = element_text(face = "bold", hjust = 0.7, size = 10, angle = 90))
+
+#ggplot(byPlaceName, aes(place_name, promedio, fill = cantidad, label = promedio),scale="free") +
+ # geom_bar(stat = "identity",position = "dodge", width=0.6) + 
+  
+  #TITULOS en los ejes y del plot
+  
+  #xlab("Localidad") +
+  #ylab("Promedio en miles de Dolares") +
+  #ggtitle("Localidades seg??n promedio de precio y cantidad de ventas") + 
+  
+  ##HASTA ACA ES UN PLOT BASICO
+  #AGREGO TEMAS
+  
+  #theme(axis.text.x = element_text(face="bold",hjust = 0.7,size=10,angle=60), 
+   #     axis.ticks = element_blank(),
+    #    plot.title = element_text(size=16, family="Noto Serif Lao", lineheight=.8, face="bold",hjust = -0.2,
+     #                             margin = margin(t = 5, r = 3, b = 20, l = 0)),
+      #  axis.line = element_blank(),
+       # panel.grid.major = element_blank(),
+        #panel.grid.minor = element_blank(),
+        #panel.border = element_blank(),
+        #panel.background = element_blank(),
+        #axis.text.y = element_blank(),
+        #axis.title = element_text(size = 11, family="Noto Serif Lao", face = "bold"),
+        #legend.title = element_text(face='bold'),
+        #axis.title.x = element_text( margin = margin(t = 5, r = -10, b = 4, l = 0))
+  #) +
+  #scale_fill_gradient(high="#0E3982",low="#D4E4FF", name="N??Publicaciones") + 
+  
+  # LE AGREGO LOS VALORES EN LA PARTE SUPERIOR DE LA BARRA
+  
+  #geom_text(data = byPlaceName,
+   #         aes(y = promedio), 
+    #        vjust = -0.3,
+     #       color = "#000066",
+      #      fontface = "bold",
+       #     size=3.5,
+        #    angle = 0,
+         #   hjust = 0.5
+  #)
